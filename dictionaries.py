8ce06eef43ab9ae166c2741c333f0eaf51117a3a
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-12 12:54:33 +0100

    hhh

---
+++ dictionaries.py
@@ -0,0 +1,39 @@
+cars = []
+car_class = ["Market segments"]
+city_car = {
+    "name": "BMW",
+    "car_class": "A" 
+    }
+
+supermini = {
+    "name": "NISSAN",
+    "car_class": "B"
+}
+
+small_family = {
+    "name": "Renault",
+    "car_class": "C"
+}
+
+large_family = {
+    "name": "Chrysler",
+    "car_class": "D"
+}
+
+executive = {
+    "name": "Mercedes",
+    "car_class": "E"
+}
+
+luxury_saloon = {
+    "name": "Lexus",
+    "car_class": "F"
+}
+
+cars.append(city_car)
+cars.append(supermini)
+cars.append(small_family)
+cars.append(large_family)
+cars.append(executive)
+cars.append(luxury_saloon)
+print(cars)
