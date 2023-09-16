a902aa68b2442f63ae17eee3538327c8f07d81dc
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-16 09:02:12 +0100

    hhh

---
+++ Classes/vehicle.py
@@ -0,0 +1,10 @@
+class Bike:
+    name = ""
+bike1 = Bike()
+bike1.name = "ZZK bike"
+print(f"Name: {bike1.name} ")
+class Cars:
+    name = ""
+car1 = Cars()
+car1.name = "Mercedes"
+print(f" Name: {car1.name}")
