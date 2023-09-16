29b7b386974446c6eaae9823915e9b6b0a78e0ae
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-16 10:36:43 +0100

    yahooo

--- logparser/libs/SysLogParser.py
+++ logparser/libs/SysLogParser.py
@@ -1,7 +1,17 @@
-import re
+from libs.utils import readFile
+import datetime
 
 class SysLogParser(object):
-    log_file_path = 'syslog.example'
+    log_file_path = '/var/log/syslog'
+    keywords = [
+        'fatal',
+        'failed',
+        'error'
+    ]
+
+    keywords_users = [
+        'root'
+    ]
 
     def info(self):
         return "This is syslog parser!"
@@ -10,8 +20,30 @@ class SysLogParser(object):
         return self.log_file_path
 
     def readFile(self):
-        with open(self.log_file_path) as log_file:
-            for line in log_file:
-                r = re.compile(r'^(?P<month>\S{3})? {1,2}(?P<day>\S+) (?P<time>\S+) (?P<hostname>\S+) (?P<process>.+?(?=\[)|.+?(?=))[^a-zA-Z0-9](?P<pid>\d{1,7}|)[^a-zA-Z0-9]{1,3}(?P<info>.*)$')
-                for match in r.finditer(line):
-                    print(match.groupdict())
+        matched_events = []
+
+        lines = readFile(self.log_file_path)
+        for log_dict in lines:
+            for keyword in self.keywords_users:
+                if keyword in log_dict['info']:
+                    # print(
+                    #     '''----log----
+                    #         date: {}
+                    #         message: {}
+                    #         '''.format(
+                    #     '{} {} {}'.format(log_dict['month'], log_dict['day'], log_dict['time']),
+                    #     log_dict['info']
+                    # ))
+
+                    # Sep 15 19:39:01
+                    date = '{} {} {} {}'.format(datetime.datetime.now().year, log_dict['month'], log_dict['day'], log_dict['time'])
+                    # print(date)
+                    formatted_date = datetime.datetime.strptime(date, "%Y %b %d %H:%M:%S")
+                    timestamp = datetime.datetime.timestamp(formatted_date)
+                    # print(timestamp)
+
+                    matched_events.append(
+                        {'date': timestamp, 'source': self.log_file_path, 'event': log_dict['info']}
+                    )
+
+        return matched_events
--- logparser/main.py
+++ logparser/main.py
@@ -1,13 +1,45 @@
 from libs.AuthLogParser import AuthLogParser
 from libs.SysLogParser import SysLogParser
+from datetime import datetime
+import time
+
 
 authLogParser = AuthLogParser()
 authLogParser.info()
-# authLogParser.readFile()
+authEvents = authLogParser.readFile()
 
 
-# print(SysLogParser.log_file_path)
 sysLogParser = SysLogParser()
-sysLogParser.readFile()
-# print(sysLogParser.info())
-# print(sysLogParser.getLogFilePath())
+sysLogEvents = sysLogParser.readFile()
+
+
+commonLog = authEvents + sysLogEvents
+
+
+sortedLog = sorted(commonLog, key=lambda d: d['date'])
+for event in sortedLog:
+    event['date'] = datetime.utcfromtimestamp(event['date']).strftime("%Y %b %d %H:%M:%S")
+    # print(event)
+
+
+mySet = set()
+myList = []
+
+for event in sortedLog:
+    mySet.add(event['event'])
+    myList.append(event['event'])
+
+
+st = datetime.now()
+if 'pam_unix(cron:session): session closed for user root' in mySet:
+    print('found event in set')
+et = datetime.now()
+
+print("execution time in set of size {} :".format(len(mySet)), (et-st).total_seconds()* 10**3)
+
+st = datetime.now()
+if 'pam_unix(cron:session): session closed for user root' in myList:
+    print('found event in list')
+et = datetime.now()
+
+print("execution time in a list of size {} :".format(len(myList)), (et-st).total_seconds()* 10**3)
