1462ccdd4db38e1df994176b9465712236a86577
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-16 09:09:22 +0100

    fff

---
+++ Classes/calc.py
@@ -0,0 +1,11 @@
+class Calc:
+    name = ""
+    type = ""
+calc1 = Calc()
+calc1.type = "Simple"
+calc1.name = "Kalkulators"
+print(f"Name: {calc1.name}, veids: {calc1.type}")
+calc2 = Calc()
+calc2.type = "Scientific"
+calc2.name = "Kalkulators"
+print(f"Name: {calc2.name}, veids: {calc2.type}")
