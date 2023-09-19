1027aa2b01831fc437f1293db748fb4f21109bce
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-19 14:38:49 +0100

    possibe

---
+++ os.traveler.py
@@ -0,0 +1,26 @@
+
+import json
+import datetime
+file_name = "/var/log/dpkg.log"
+file = open(file_name, "r")
+data = []
+order = ["Date", "Time", "Triger", "Task", "Package", "Version"]
+
+for line in file.readlines():
+    details = line.split(" ")
+    details = [x.strip() for x in details]
+    structure = {key:value for key, value in zip(order, details)}
+    data.append(structure)
+save_file = open("/home/parrot/pysec2023/1.json", "w")
+json.dump(data,save_file,indent = 6)
+save_file.close()
+fp = open("/home/parrot/pysec2023/1.json")
+data = json.load(fp)
+#dt = datetime.datetime.now()
+#dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+#json_data = json.dumps(dt_str)
+#print(json_data)
+#print("Printing incident date, time and package: \n", data)
+content = fp.read("Date")
+print(content)
+#print(data['Date'][0]['Time'][0]['Package'])
---
+++ traveler/main.py
@@ -0,0 +1,22 @@
+import os
+# print("Current directory is: ", os.getcwd())
+# print(os.listdir(os.getcwd()))
+# print("Changing directory: ", os.chdir("/home/parrot/"))
+# print(os.listdir(os.getcwd()))
+
+from pathlib import Path
+space =  '    '
+branch = '│   '
+# pointers:
+tee =    '├── '
+last =   '└── '
+def tree(dir_path: Path, prefix: str=''):
+    contents = list(dir_path.iterdir())
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():
+            extension = branch if pointer == tee else space 
+            yield from tree(path, prefix=prefix+extension)
+for line in tree(Path.home() / '/home/parrot/pysec2023'):
+    print(line)
