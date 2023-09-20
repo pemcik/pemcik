7e2b44377177610cb512dbbd3f457ea6de9bcc61
pemcik <dmitrijs.sinelnikovs@gmail.com>
2023-09-20 14:44:41 +0100

    works as hell

--- traveler/test.py
+++ traveler/test.py
@@ -1,19 +1,14 @@
 import os
-
-from pathlib import Path
-
-space =  '    '
-branch = '│   '
-tee =    '├── '
-last =   '└── '
-
-def tree(dir_path: Path, prefix: str=''):
-    contents = list(dir_path.iterdir())
-    pointers = [tee] * (len(contents) - 1) + [last]
-    for pointer, path in zip(pointers, contents):
-        yield prefix + pointer + path.name 
-        if path.is_dir():
-            extension = branch if pointer == tee else space 
-            yield from tree(path, prefix=prefix+extension)
-for line in tree(Path.home() / '/home/parrot/pysec2023/'):
-    print(line)
+for dirname, dirnames, filenames in os.walk('.'):
+    for subdirname in dirnames:
+        dirnames.sort()
+        print(os.path.join(dirname, subdirname))
+    for filename in filenames:
+        filenames.sort()
+        print(os.path.join(dirname, filename))
+    if '.git' in dirnames:
+        dirnames.remove('.git')
+    if 'venv3.9' in dirnames:
+        dirnames.remove('venv3.9')
+    
+    
